d3dfc48b0fe31db4751887d45943b8318e231a2b
wrrouman <106677200+wrrouman@users.noreply.github.com>
2023-03-05 08:26:22 -0500

    Fix cors

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://tradebycolours.herokuapp.com",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
