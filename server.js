850f009b7ac5c9cfce5b4f316bd43861b48f72b0
wrrouman <106677200+wrrouman@users.noreply.github.com>
2023-03-05 08:45:31 -0500

    Fix cors

--- server.js
+++ server.js
@@ -18,7 +18,25 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors());
+// app.use(cors());
+
+app.use(function (req, res, next) {
+  const allowedOrigins = ["https://tradebycolours.herokuapp.com"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.use("/auth", authRoute);
 
